ddcffaa951ad730956c89ac5f7c693e153db6a9a
lc <459916290@qq.com>
2018-09-26 19:44:26 +0800

    11

--- git/src/main/java/com/luocheng/test.java
+++
@@ -1,8 +0,0 @@
-package com.luocheng;
-public class test {
-    public static void main(String[] args) {
-        System.out.println("1111111");
-		System.out.println("2222222");
-		System.out.println("4444444");
-    }
-}
