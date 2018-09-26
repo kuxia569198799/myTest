41d0c82a1db0844353529d1636ea08959a1c8b55
lc <459916290@qq.com>
2018-09-26 16:37:44 +0800

    B第一次提交

--- git/src/main/java/com/luocheng/test.java
+++ git/src/main/java/com/luocheng/test.java
@@ -3,5 +3,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("1111111");
 		System.out.println("2222222");
+		System.out.println("4444444");
     }
 }
